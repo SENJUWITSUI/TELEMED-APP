4987f0a345d96934fd00c118a044929d05141b4a
SENJUWITSUI <jonellecardino@gmailcom>
2023-02-27 16:42:51 +0800

    fragments

---
+++ app/src/main/java/com/example/telemed/Myviewpager.java
@@ -0,0 +1,35 @@
+package com.example.telemed;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.telemed.fragments.Aktibo;
+import com.example.telemed.fragments.Natapos;
+import com.example.telemed.fragments.Aktibo;
+import com.example.telemed.fragments.Natapos;
+
+public class Myviewpager extends FragmentStateAdapter {
+    public Myviewpager(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new Aktibo();
+            case 1:
+                return new Natapos();
+            default:
+                return new Aktibo();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
