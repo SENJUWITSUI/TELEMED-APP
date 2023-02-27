62776d93f4528f26dfdeec4c19120862e4e4cafa
SENJUWITSUI <jonellecardino@gmailcom>
2023-02-27 13:16:37 +0800

    user interface

---
+++ app/src/main/java/com/example/telemed/User_Interface.java
@@ -0,0 +1,14 @@
+package com.example.telemed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_Interface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_interface);
+    }
+}
---
+++ app/src/main/java/com/example/telemed/header1.java
@@ -0,0 +1,14 @@
+package com.example.telemed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class header1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_header1);
+    }
+}
