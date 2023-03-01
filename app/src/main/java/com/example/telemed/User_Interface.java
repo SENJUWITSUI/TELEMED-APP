5974ad75bdf9add28e174bcbb81f1813010d28b6
SENJUWITSUI <jonellecardino@gmailcom>
2023-03-01 14:02:42 +0800

    reg

--- app/src/main/java/com/example/telemed/User_Interface.java
+++ app/src/main/java/com/example/telemed/User_Interface.java
@@ -1,14 +1,113 @@
 package com.example.telemed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class User_Interface extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    Myviewpager myviewpager;
+
+    private Button button;
+    TextView txt;
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_interface);
+
+        tabLayout = findViewById(R.id.tabs);
+        viewPager2 = findViewById(R.id.view1);
+        myviewpager = new Myviewpager(this);
+        viewPager2.setAdapter(myviewpager);
+
+        dialog = new Dialog(User_Interface.this);
+        dialog.setContentView(R.layout.activity_pop_up_window);
+
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+        dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
+
+        button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
+
+        Button hindi = dialog.findViewById(R.id.btn_hindi);
+        Button ipasa = dialog.findViewById(R.id.btn_ipasa);
+
+        hindi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openmain();
+            }
+        });
+
+        txt = (TextView) dialog.findViewById(R.id.exit);
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
+
+
+        ipasa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opencalendar();
+
+            }
+        });
+
+    }
+
+    public void openmain() {
+        Intent intent = new Intent(this, User_Interface.class);
+        startActivity(intent);
+    }
+    public void opencalendar() {
+        Intent intent = new Intent(this, Calendar.class);
+        startActivity(intent);
     }
 }
