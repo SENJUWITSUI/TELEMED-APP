2a576f7092b9bd4767d68236736c20dd6c890929
prbn19 <arbanprincess66@gmail.com>
2023-03-01 16:40:21 +0800

    SuperAdmin_Hospitals

---
+++ app/src/main/java/com/example/telemed/SuperAdmin_Hospitals.java
@@ -0,0 +1,14 @@
+package com.example.telemed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuperAdmin_Hospitals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_admin_hospitals);
+    }
+}
